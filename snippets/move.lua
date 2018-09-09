a7926d7488579d87aa46ee9f3d8ad58b54ad4e5e
Kevin Gisi <kevin@kevingisi.com>
2018-09-08 22:03:21 -0500

    Add offset tracking to move

--- snippets/move.lua
+++ snippets/move.lua
@@ -1,25 +1,64 @@
+positionOffset = { 0, 0, 0 }
+rotationOffset = 0
 function move(path, f)
   local bot = component.proxy(
     component.list("robot")() or component.list("drone")()
   )
-  local move_dir = {
-    l = bind(bot.turn, false),
-    r = bind(bot.turn, true),
-    f = bind(bot.move, 3),
-    b = bind(bot.move, 2),
-    u = bind(bot.move, 1),
-    d = bind(bot.move, 0),
-    y = f
-  }
+  local perform = {}
+  function perform.l() 
+    if bot.turn(false) then
+      rotationOffset = (rotationOffset - 1) % 4
+    end
+  end
+  function perform.r() 
+    if bot.turn(true) then
+      rotationOffset = (rotationOffset + 1) % 4
+    end
+  end
+  function perform.f()
+    if bot.move(3) then
+      offsetCurrentDirection(1)
+    end
+  end
+  function perform.b()
+    if bot.move(2) then
+      offsetCurrentDirection(-1)
+    end
+  end
+  function perform.u()
+    if bot.move(1) then
+      positionOffset[2] = positionOffset[2] + 1
+    end
+  end
+  function perform.d()
+    if bot.move(0) then
+      positionOffset[2] = positionOffset[2] - 1
+    end
+  end
+  perform.y = f
   for step in string.gmatch(path, "%d*%a") do
-    local quantity = string.match(step, "%d+") or 1
-    local direction = string.sub(step, -1)
-    for i = 1,quantity do 
-      move_dir[direction]()
+    local n = string.match(step, "%d+") or 1
+    local dir = string.sub(step, -1)
+    for i = 1,n do 
+      perform[dir]()
     end
   end
 end
 
-function bind(f, arg)
-  return function() f(arg) end
+function offsetCurrentDirection(i)
+  if rotationOffset == 0 then
+    positionOffset[1] = positionOffset[1] + i
+  elseif rotationOffset == 1 then
+    positionOffset[3] = positionOffset[3] + i
+  elseif rotationOffset == 2 then
+    positionOffset[1] = positionOffset[1] - i
+  else
+    positionOffset[3] = positionOffset[3] - i
+  end
 end
+
+-- move("urflurflurflurflurfl")
+-- print("Rotation: "..rotationOffset)
+-- print("X: "...positionOffset[1])
+-- print("Y: "...positionOffset[2])
+-- print("Z: "...positionOffset[3])
