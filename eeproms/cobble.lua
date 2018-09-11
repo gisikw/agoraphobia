37a5c7bb24a68365fa9083c3aeeb7254f61cfc97
Kevin Gisi <kevin@kevingisi.com>
2018-09-10 23:24:10 -0500

    Add cobblestone miner eeprom

---
+++ eeproms/cobble.lua
@@ -0,0 +1,29 @@
+local robot = component.proxy(component.list("robot")())
+local STOPPING_DURABILITY = 0.1
+
+function sleep(timeout)
+  local deadline = computer.uptime() + (timeout or 0)
+  repeat
+    computer.pullSignal(deadline - computer.uptime())
+  until computer.uptime() >= deadline
+end
+
+while true do
+  while true do
+    local durability = robot.durability()
+    if durability == nil or durability < STOPPING_DURABILITY then
+      break
+    end
+    if robot.detect() then
+      robot.swing()
+    end
+    sleep(0.2)
+  end
+  while true do
+    local durability = robot.durability()
+    if durability ~= nil and durability > STOPPING_DURABILITY then
+      break
+    end
+    sleep(5)
+  end
+end
