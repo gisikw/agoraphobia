48083d0de52cfe1377d796f842a7b8d5568b9c56
Kevin Gisi <kevin@kevingisi.com>
2018-09-12 11:10:46 -0500

    Wrap up plant script

--- eeproms/plant.lua
+++ eeproms/plant.lua
@@ -2,40 +2,45 @@ local drone = component.proxy(component.list("drone")())
 local inv = component.proxy(component.list("inventory_controller")())
 local modem = component.proxy(component.list("modem")())
 
+local FARM_FREQUENCY = 40 -- Minutes
+local PORT = 4723
+
 function main()
-  sleep(10)
-  moveToAboveChest()
-  drone.select(1)
-  for i = 28,45 do
-    local hasCount = drone.count()
-    if hasCount == 64 then
-      local currentSelect = drone.select()
-      if currentSelect == 8 then
-        break
-      else
-        drone.select(currentSelect + 1)
+  while true do
+    moveToAboveChest()
+    drone.select(1)
+    for i = 28,45 do
+      local hasCount = drone.count()
+      if hasCount == 64 then
+        local currentSelect = drone.select()
+        if currentSelect == 8 then
+          break
+        else
+          drone.select(currentSelect + 1)
+        end
+      end
+      local available = inv.getStackInSlot(0, i).size
+      if available > 1 then
+        inv.suckFromSlot(0, i, math.min(available - 1, 64 - hasCount))
       end
     end
-    local available = inv.getStackInSlot(0, i).size
-    if available > 1 then
-      inv.suckFromSlot(0, i, math.min(available - 1, 64 - hasCount))
-    end
+    drone.select(1)
+    ascendToFarm()
+    local startTime = computer.uptime()
+    repeat
+      modem.broadcast(PORT)
+      sleep(0.2)
+    until computer.uptime() > startTime + 2
+    sleep(10)
+    navigateFarmLayout(function()
+      if drone.count() == 0 then
+        drone.select(drone.select() + 1)
+      end
+      drone.place(0)
+    end)
+    returnToCharger()
+    sleep(60 * FARM_FREQUENCY)
   end
-  drone.select(1)
-  ascendToFarm()
-  local startTime = computer.uptime()
-  repeat
-    modem.broadcast(4723)
-    sleep(0.2)
-  until computer.uptime() > startTime + 2
-  sleep(10)
-  navigateFarmLayout(function()
-    if drone.count() == 0 then
-      drone.select(drone.select() + 1)
-    end
-    drone.place(0)
-  end)
-  returnToCharger()
 end
 
 function moveToAboveChest()
@@ -65,15 +70,14 @@ end
 function returnToCharger()
   move(-6, 0, -6)
   move(0, -4, 0)
-  move(0, 0, -2)
+  move(0, 0, -1)
 end
 
 function move(x, y, z, timeout)
   drone.move(x, y, z)
-  local deadline = computer.uptime() + (timeout or 5)
   repeat
     sleep(0.1)
-  until drone.getOffset() < 0.4 or drone.getVelocity() == 0 or computer.uptime() >= deadline
+  until drone.getOffset() < 0.4 or drone.getVelocity() == 0
 end
 
 function sleep(timeout)
