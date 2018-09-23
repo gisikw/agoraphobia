8baf772120f03d793ba6296b5c9601050c2b7f6b
Kevin Gisi <kevin@kevingisi.com>
2018-09-22 23:15:45 -0500

    Rewrite bundle using two passes

--- bin/bundle.lua
+++ bin/bundle.lua
@@ -2,47 +2,7 @@
 -- Unix Usage: lua bundle.lua file.lua output.lua
 
 local REQUIRE_PATTERN = "^(.*)require%([\"']([^\"']+)[\"']%)(.*)$"
-
-arg = {...}
-if #arg ~= 2 then
-  print "Usage: bundle input.lua output.lua"
-  return 1
-end
-local scanContent
-local defined = {}
-
-local function define(name, buffer)
-  if not defined[name] then
-    local path = package.searchpath(name, package.path)
-    local lines = scanContent(path, buffer)
-    buffer:write("bundle.__defs[\"" .. name .. "\"] = function()\n")
-    for _, line in ipairs(lines) do
-      buffer:write(line .. "\n")
-    end
-    buffer:write("end\n")
-    defined[name] = true
-  end
-end
-
-function scanContent(path, buffer)
-  local file = io.open(path)
-  local lines = {}
-  local line = file:read("*l")
-  while line do
-    line = line:gsub(REQUIRE_PATTERN, function(prefix, name, suffix)
-      define(name, buffer)
-      return prefix .. "bundle[\"" .. name .. "\"]" .. suffix
-    end)
-    table.insert(lines, line)
-    line = file:read("*l")
-  end
-  file:close()
-  return lines
-end
-
-local function main()
-  local buffer = io.open(arg[2], "w")
-  buffer:write([[bundle = { __defs = { } }
+local HEADER = [[bundle = { __defs = { } }
 setmetatable(bundle, {
   __index = function(self, key)
     local result
@@ -53,13 +13,53 @@ setmetatable(bundle, {
     end
   end
 })
-]])
-  local lines = scanContent(arg[1], buffer)
-  buffer:write("\n")
-  for _, line in ipairs(lines) do
-    buffer:write(line .. "\n")
+]]
+
+arg = {...}
+if #arg ~= 2 then
+  print "Usage: bundle input.lua output.lua"
+  return 1
+end
+
+local function parseDependencyTree(paths, deps)
+  if #paths == 0 then return deps end
+  local file = io.open(table.remove(paths, 1))
+  local line = file:read("*l")
+  while line do
+    line:gsub(REQUIRE_PATTERN, function(prefix, name, suffix)
+      if not deps[name] then
+        local path = package.searchpath(name, package.path)
+        table.insert(paths, path)
+        deps[name] = path
+      end
+    end)
+    line = file:read("*l")
   end
-  buffer:close()
+  file:close()
+  return parseDependencyTree(paths, deps)
+end
+
+local function appendFile(file, buffer)
+  local line = file:read("*l")
+  while line do
+    buffer:write(line:gsub(REQUIRE_PATTERN, function(prefix, name, suffix)
+      return prefix .. "bundle[\"" .. name .. "\"]" .. suffix
+    end) .. "\n")
+    line = file:read("*l")
+  end
+  file:close()
+end
+
+local function main()
+  dependencies = parseDependencyTree({ arg[1] }, {})
+  local buffer = io.open(arg[2], "w")
+  buffer:write(HEADER)
+  for name, path in pairs(dependencies) do
+    buffer:write("bundle.__defs[\"" .. name .. "\"] = function()\n")
+    appendFile(io.open(path), buffer)
+    buffer:write("end\n")
+  end
+  appendFile(io.open(arg[1]), buffer)
 end
 
 main()
