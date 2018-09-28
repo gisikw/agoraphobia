de226f4a10c2cea16071d002478ebb6fa03e94c8
Kevin Gisi <kevin@kevingisi.com>
2018-09-27 22:27:10 -0500

    Add add, reset, status to oaf

--- bin/oaf.lua
+++ bin/oaf.lua
@@ -1,47 +1,111 @@
 local serialization = require("../src/vendor/openos/serialization")
+local github = require("../src/lib/github")
 
 local arg = {...}
 local cmd = {}
 
-function cmd.help()
-  print("usage: coming soon!")
-end
-
-function pullTree(meta, sha, dir, pathPrefix)
+local function pullTree(meta, sha, dir, pathPrefix)
   os.execute("mkdir -p " .. dir)
-  local https = io.popen("curl -s https://api.github.com/repos/" ..  meta.user ..  "/" ..  meta.repo ..  "/git/trees/" .. sha)
-  local result = https:read("*a"):match("\"tree\": %[([^%]]+)%]")
-  https:close()
-  local entries = {}
-  for jsonEntry in result:gmatch("{([^}]+)}") do
-    table.insert(entries, {
-      path = jsonEntry:match("\"path\": \"([^\"]+)\""),
-      type = jsonEntry:match("\"type\": \"([^\"]+)\""),
-      sha = jsonEntry:match("\"sha\": \"([^\"]+)\"")
-    })
-  end
+  local entries = github.entriesInTree(meta.shortname, sha)
   for _, entry in ipairs(entries) do
     if entry.type == "blob" then
-      os.execute("wget https://raw.githubusercontent.com/" ..  meta.user ..  "/" ..  meta.repo ..  "/master/" .. pathPrefix .. entry.path .. " -O " .. dir .. "/" .. entry.path)
+      github.getBlob(meta.shortname, pathPrefix .. entry.path, dir .. "/" .. entry.path)
     else
       pullTree(meta, entry.sha, dir .. "/" .. entry.path, pathPrefix .. entry.path .. "/")
     end
   end
 end
 
+local function getMeta(dir)
+  local dir = dir or "."
+  return serialization.unserialize(io.open(dir .. "/.oaf"):read("*a"))
+end
+
+local function setMeta(meta, dir)
+  local dir = dir or "."
+  local metafile = io.open(dir .. "/.oaf", "w")
+  metafile:write(serialization.serialize(meta))
+  metafile:close()
+end
+
+function cmd.help()
+  print("usage: coming soon!")
+end
+
+function cmd.commit(arg)
+  local meta = getMeta(dir) 
+  if not next(meta.staged) then
+    print "No staged changes to commit"
+    return 1
+  end
+  local changes = {}
+  for path, _ in pairs(meta.staged) do
+    table.insert(changes, {
+      path = path,
+      mode = "100644",
+      type = "blob",
+      content = io.open(path):read("*a")
+    })
+  end
+  github.commit(changes)
+end
+
+function cmd.status()
+  local dir = arg.dir or "./agoraphobia" -- FIXME
+  local meta = getMeta(dir)
+  if next(meta.staged) then
+    print("Files staged for commit:")
+    for file, _ in pairs(meta.staged) do
+      print("- " .. file)
+    end
+  else
+    print("No files staged for commit")
+  end
+end
+
+-- TODO Features: 
+-- - Check if file exists
+-- - If a directory, add everything in that dir
+function cmd.add(arg)
+  local dir = arg.dir or "./agoraphobia" -- FIXME
+  local meta = getMeta(dir)
+  meta.staged[arg[2]] = true
+  setMeta(meta, dir)
+  print(arg[2] .. " staged for commit")
+end
+
+-- TODO Features: 
+-- - Check if file exists
+-- - If a directory, add everything in that dir
+function cmd.reset(arg)
+  local dir = arg.dir or "./agoraphobia" -- FIXME
+  local meta = getMeta(dir)
+  if arg[2] then
+    meta.staged[arg[2]] = nil
+    print(arg[2] .. " unstaged")
+  else
+    meta.staged = {}
+    print("All files unstaged")
+  end
+  setMeta(meta, dir)
+end
+
 function cmd.pull(arg)
   local dir = arg.dir or "."
-  local meta = serialization.unserialize(io.open(dir .. "/.oaf"):read("*a"))
+  local meta = getMeta(dir)
   pullTree(meta, "master", dir, "")
 end
 
 function cmd.clone(arg)
   user, repo = arg[2]:match("([^/]+)/(.+)")
   os.execute("mkdir " .. repo)
-  local metafile = io.open(repo .. "/.oaf", "w")
-  local meta = { user = user, repo = repo }
-  metafile:write(serialization.serialize(meta))
-  metafile:close()
+  local meta = 
+  setMeta({ 
+    user = user, 
+    repo = repo, 
+    shortname = arg[2], 
+    staged = {} 
+  }, repo)
   cmd.pull({ dir = repo })
 end
 
